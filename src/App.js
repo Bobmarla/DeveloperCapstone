7dbf2ab731ccc7bb08d91c2ff7ceb70dda79449e
Maral S <maral.s@tavanbogd.com>
2024-10-31 12:53:10 -0500

    fuuuuck

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-	return <div className="App">hi bob bob</div>;
+	return <div className="App">hi shit </div>;
 }
 
 export default App;
