bcb6a6bbe7367e788b363ea773b264aa16e47324
Maral S <maral.s@tavanbogd.com>
2024-10-31 12:51:05 -0500

    commit ffs

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-	return <div className="App">hi bob</div>;
+	return <div className="App">hi bob bob</div>;
 }
 
 export default App;
